e4faa0188e0900fc5f7c701d3342242bae5edfde
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-12-26 19:49:09 +0330

    Build the navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="mb-5 flex h-14 items-center space-x-6 border-b px-5">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-zinc-500 transition-colors hover:text-zinc-800"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
