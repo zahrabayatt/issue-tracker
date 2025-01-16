71aca336ad2932c1154205e4990080e75d7e4d75
Zahra Bayat <zahra.bayat13799@gmail.com>
2025-01-16 17:15:30 +0330

    Disable SSR for loading markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,7 +2,7 @@
 
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import { Controller, useForm } from "react-hook-form";
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -12,6 +12,10 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import { ErrorMessage } from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
