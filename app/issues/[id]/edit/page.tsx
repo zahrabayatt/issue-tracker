4faac5b1ea630a20a1c73cfe947baa996f4ed038
Zahra Bayat <zahra.bayat13799@gmail.com>
2025-01-17 13:02:38 +0330

    Build the edit issue page

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import IssueForm from "../../_components/IssueForm";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+const EditIssuePage = async ({ params }: Props) => {
+  const { id } = await params;
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!issue) notFound();
+
+  return <IssueForm issue={issue} />;
+};
+
+export default EditIssuePage;
