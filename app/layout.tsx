abaec161abd7676d2a8ad4ac10df6246ef6385e2
Zahra Bayat <zahra.bayat13799@gmail.com>
2025-02-20 18:48:20 +0330

    Add a delete button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "@radix-ui/themes/styles.css";
 import "./theme-config.css";
 import "./globals.css";
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
@@ -28,7 +28,9 @@ export default function RootLayout({
         <AuthProvider>
           <Theme accentColor="violet">
             <NavBar />
-            <main className="p-5">{children}</main>
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
           </Theme>
         </AuthProvider>
       </body>
