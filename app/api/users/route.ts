06cb54a40ec1d15b131c08890dd47672c88bfba1
Zahra Bayat <zahra.bayat13799@gmail.com>
2025-02-21 21:15:40 +0330

    Populate the assignee select component

---
+++ app/api/users/route.ts
@@ -0,0 +1,10 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+// add the request parm to prevent caching this endpoint
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany({ orderBy: { name: "asc" } });
+
+  return NextResponse.json(users);
+}
