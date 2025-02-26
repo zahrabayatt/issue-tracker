96f33adb3978d3e7effec5d0c5f062b8515a72bf
Zahra Bayat <zahra.bayat13799@gmail.com>
2025-02-26 17:22:06 +0330

    Optimize performance using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,18 +7,23 @@ import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
+import { cache } from "react";
 
 interface Props {
   params: Promise<{ id: string }>;
 }
 
+const fetchUser = cache((issueId: number) =>
+  prisma.issue.findUnique({
+    where: { id: issueId },
+  }),
+);
+
 const IssueDetailPage = async (props: Props) => {
   const session = await getServerSession(authOptions);
   const { id } = await props.params;
 
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(id) },
-  });
+  const issue = await fetchUser(parseInt(id));
 
   if (!issue) {
     notFound();
@@ -46,7 +51,7 @@ export default IssueDetailPage;
 
 export async function generateMetadata({ params }: Props) {
   const { id } = await params;
-  const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
+  const issue = await fetchUser(parseInt(id));
 
   return {
     title: issue?.title,
--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -1,18 +1,22 @@
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import EditIssue from "./EditIssue";
+import { cache } from "react";
 
 interface Props {
   params: Promise<{ id: string }>;
 }
 
+const fetchUser = cache((issueId: number) =>
+  prisma.issue.findUnique({
+    where: { id: issueId },
+  }),
+);
+
 const EditIssuePage = async ({ params }: Props) => {
   const { id } = await params;
 
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(id) },
-  });
-
+  const issue = await fetchUser(parseInt(id));
   if (!issue) notFound();
 
   return <EditIssue issue={issue} />;
@@ -22,9 +26,7 @@ export default EditIssuePage;
 
 export async function generateMetadata({ params }: Props) {
   const { id } = await params;
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(id) },
-  });
+  const issue = await fetchUser(parseInt(id));
 
   return {
     title: issue?.title ? `Edit Issue: ${issue.title}` : "Edit Issue",
